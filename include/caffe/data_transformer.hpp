bf4a4663f5f217ee7f67e60da04d5fe949034de9
wistone <shijianping5000@gmail.com>
2015-06-22 17:52:56 +0800

    add image data layer from gpapan

--- include/caffe/data_transformer.hpp
+++ include/caffe/data_transformer.hpp
@@ -88,6 +88,10 @@ class DataTransformer {
    */
   void Transform(Blob<Dtype>* input_blob, Blob<Dtype>* transformed_blob);
 
+  void TransformImgAndSeg(const std::vector<cv::Mat>& cv_img_seg,
+		      Blob<Dtype>* transformed_data_blob, Blob<Dtype>* transformed_label_blob,
+			      const int ignore_label);
+  
   /**
    * @brief Infers the shape of transformed_blob will have when
    *    the transformation is applied to the data.
